26a130e0ce19af5c7eacd978a2f6f607ea6b311a
Diggle Dollarz <eddie@diggle.tech>
2017-05-25 10:59:38 +0200

    Create the model

--- TeamProjectManager/Models/IdentityModels.cs
+++ TeamProjectManager/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,6 +10,10 @@ namespace TeamProjectManager.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public List<Project> Projects{ get; set; }
+        public List<Task> Tasks { get; set; }
+
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -16,6 +21,8 @@ namespace TeamProjectManager.Models
             // Add custom user claims here
             return userIdentity;
         }
+
+
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
