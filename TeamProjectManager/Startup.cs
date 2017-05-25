82e6fbb1bfb81354d3e3b9f508f60c418cbe2a22
Diggle Dollarz <eddie@diggle.tech>
2017-05-25 09:20:29 +0200

    Add project files.

---
+++ TeamProjectManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TeamProjectManager.Startup))]
+namespace TeamProjectManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
